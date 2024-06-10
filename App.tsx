9d16188985ad2169e36a319162537b50dcb1d95f
Tim <ju4nshin@gmail.com>
2024-06-10 22:51:26 +0900

    refactor : theme -> colors로 파일명 변경

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
 import {EvaIconsPack} from '@ui-kitten/eva-icons';
 import {NavigationContainer} from '@react-navigation/native';
 import RootNavigator from './src/navigation/root/RootNavigaitor';
-import {default as themeJson} from './src/styles/theme.json';
+import {default as color} from './src/styles/colors.json';
 import {ThemeContext} from './src/context/theme-context';
 
 function App(): React.JSX.Element {
@@ -19,7 +19,7 @@ function App(): React.JSX.Element {
     <>
       <IconRegistry icons={EvaIconsPack} />
       <ThemeContext.Provider value={{theme, toggleTheme}}>
-        <ApplicationProvider {...eva} theme={{...eva[theme], ...themeJson}}>
+        <ApplicationProvider {...eva} theme={{...eva[theme], ...color}}>
           <NavigationContainer>
             <RootNavigator />
           </NavigationContainer>
