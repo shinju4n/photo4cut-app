a9e0d8b55688594f3b93c1c1161ac0d7b22cebe8
Tim <ju4nshin@gmail.com>
2024-06-11 19:10:49 +0900

    feat : 네컷 추가 레이아웃 구현

--- src/screens/album/AddMediaScreen.tsx
+++ src/screens/album/AddMediaScreen.tsx
@@ -1,17 +1,69 @@
 import React from 'react';
-import {Layout, Text} from '@ui-kitten/components';
-import {SafeAreaView} from 'react-native';
+import {ImageProps, SafeAreaView, StyleSheet} from 'react-native';
+import {AlbumStackParamList} from '@/navigation/stack/AlbumNavigator';
+import {type StackScreenProps} from '@react-navigation/stack';
+import {Button, IconElement, Layout} from '@ui-kitten/components';
+import TopHeader from '@/components/TopHeader';
+import CustomIcon from '@/components/CustomIcon';
+import {AlbumRoutes} from '@/constants';
 
-interface AddMediaScreenProps {}
+type AddMediaScreenProps = StackScreenProps<
+  AlbumStackParamList,
+  typeof AlbumRoutes.ADD_MEDIA
+>;
 
-const AddMediaScreen = ({}: AddMediaScreenProps) => {
+interface RenderIconProp extends Partial<ImageProps> {
+  name: string;
+}
+
+const renderIcon = ({name, ...props}: RenderIconProp): IconElement => {
+  return <CustomIcon name={name} {...props} />;
+};
+
+const AddMediaScreen = ({navigation}: AddMediaScreenProps) => {
   return (
-    <SafeAreaView>
-      <Layout>
-        <Text>AddMediaScreen</Text>
+    <SafeAreaView style={styles.container}>
+      <TopHeader
+        title="네컷 추가"
+        leftAction={{
+          iconName: 'arrow-back',
+          onPress: () => navigation.goBack(),
+        }}
+      />
+      <Layout style={styles.buttonContainer}>
+        <Button
+          style={styles.button}
+          size="giant"
+          accessoryLeft={props => renderIcon({name: 'folder', ...props})}
+          onPress={() => {}}>
+          사진에서 가져오기
+        </Button>
+        <Button
+          style={styles.button}
+          size="giant"
+          appearance="outline"
+          accessoryLeft={props => renderIcon({name: 'grid-outline', ...props})}
+          onPress={() => {}}>
+          QR 코드로 가져오기
+        </Button>
       </Layout>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 16,
+  },
+  button: {
+    width: '80%',
+  },
+});
 
 export default AddMediaScreen;
