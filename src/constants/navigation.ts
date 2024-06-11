2cad7c672edc297fd4d868c0189768c543c57122
Tim <ju4nshin@gmail.com>
2024-06-11 13:15:42 +0900

    feat : navigation constants 세팅

---
+++ src/constants/navigation.ts
@@ -0,0 +1,9 @@
+export const BottomTabRoutes = {
+  HOME: 'Home',
+  PHOTO_ALBUM: 'PhotoAlbum',
+} as const;
+
+export const PhotoAlbumRoutes = {
+  ADD_MEDIA: 'AddMedia',
+  PHOTO_ALBUM_HOME: 'PhotoAlbumHOME',
+} as const;
