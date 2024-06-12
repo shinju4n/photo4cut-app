e967442eba6bf02f5cbab6cbdc336186e2174742
Tim <ju4nshin@gmail.com>
2024-06-12 13:59:45 +0900

    feat : useUploadMedia 훅 구현

---
+++ src/hooks/useUploadMedia.ts
@@ -0,0 +1,25 @@
+import React from 'react';
+import {ImageOrVideo} from 'react-native-image-crop-picker';
+import {uploadMediaToS3} from '@/api/image';
+import {getFormDataMedia} from '@/utils';
+
+const useUploadMedia = () => {
+  const [isUploading, setIsUploading] = React.useState<boolean>(false);
+
+  const uploadToS3 = async (media: ImageOrVideo) => {
+    const formData = getFormDataMedia(media);
+    try {
+      setIsUploading(true);
+      const res = await uploadMediaToS3(formData);
+      return res;
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsUploading(false);
+    }
+  };
+
+  return {uploadToS3, isUploading};
+};
+
+export default useUploadMedia;
