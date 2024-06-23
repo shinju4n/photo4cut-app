87d2bf446da4830f226c9df8c9539ba3032388c4
shinju4n <ju4nshin@gmail.com>
2024-06-23 18:35:56 +0900

    feat : 앨범이 없을 떄 레이아웃 구현

--- src/components/album/AlbumList.tsx
+++ src/components/album/AlbumList.tsx
@@ -1,15 +1,23 @@
 import React, {FC} from 'react';
-import {Dimensions} from 'react-native';
+import {Dimensions, StyleSheet} from 'react-native';
 import {FlashList} from '@shopify/flash-list';
 import {type Album} from '@/types';
 import AlbumListItem from './AlbumListItem';
+import {Layout, Text} from '@ui-kitten/components';
 
 interface AlbumListProps {
-  // 임시 Undefined 타입을 제거하고 Album 타입으로 변경
-  data?: Album[];
+  data: Album[];
 }
 
-const AlbumList: FC<AlbumListProps> = ({data}) => {
+const AlbumList: FC<AlbumListProps> = ({data = []}) => {
+  if (data.length === 0) {
+    return (
+      <Layout style={styles.container}>
+        <Text>앨범이 없습니다.</Text>
+      </Layout>
+    );
+  }
+
   return (
     <FlashList
       data={data}
@@ -19,5 +27,13 @@ const AlbumList: FC<AlbumListProps> = ({data}) => {
     />
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default AlbumList;
